34d41dd0b0fe44854aed8a1cf4c9dac9a7986918
MidnightDev024 <chawlaakash024@gmail.com>
2025-08-04 23:55:57 +0530

    promise

--- API Request/promises.js
+++ API Request/promises.js
@@ -87,11 +87,21 @@ async function consumePromiseFive(params) {
 consumePromiseFive()
 
 
-async function getAllUsers() {
-    const response = await fetch('https://jsonplaceholder.typicode.come/users')
-    const data = response.json()
+// async function getAllUsers() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+    
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
     console.log(data);
     
-}
-
-getAllUsers()
+})
+.catch((error) => console.log(error))
---
+++ API Request/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+// function getAllUsers() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = response.json()
+//     console.log(data);
+    
+// }
+
+// getAllUsers()

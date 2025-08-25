b56a9a2eb13a66569ce2186c68d4edae3387fe1c
MidnightDev024 <chawlaakash024@gmail.com>
2025-08-25 23:56:19 +0530

    Add Navbar component for improved navigation, update index.html for SEO with new title and description, and modify App component to include Navbar.

--- react app/a-cash/src/components/Navbar.js
+++ react app/a-cash/src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode} sticky-top`}> 
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button

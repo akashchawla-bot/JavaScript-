df672e82ad9786dec4900a2a3864a3dd872f03a2
MidnightDev024 <chawlaakash024@gmail.com>
2025-08-26 23:57:59 +0530

    Add News and NewItem components to display news articles, refactor Navbar for improved styling and structure, and update App component to include News.

---
+++ react app/news-app/src/components/News.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import NewItem from './NewItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        News
+        <NewItem/>
+        <NewItem/>
+        <NewItem/>
+        <NewItem/>
+        <NewItem/>
+        <NewItem/>
+        <NewItem/>
+        <NewItem/>
+        </div>
+    )
+  }
+}
+
+export default News
